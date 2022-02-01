9b3c141ef09b4f302314f7cf92a7bf777f04edd4
ThiquidPenguiN <thiquidpenguin@gmail.com>
2022-02-01 04:11:24 -0800

    able to generate 2 item dictornary
    {'GameTitle': 'Grand Theft Auto V', 'ReviewsByScore': [5, 4, 3]}

--- giantbomb-api-parser.py
+++ giantbomb-api-parser.py
@@ -7,46 +7,116 @@ import json
 #URI Formation
 base_url = 'https://www.giantbomb.com/api'
 moduleCompany = "/company"
+moduleReviews = "/user_reviews"
 companyCodeTake2 = "/3010-450" #company code can only be acquired by navigating with a web browser to giantbomb.com, finding the company profile, and inspecting the URL.
 #query constructors
-fields = "field_list=published_games"
+publishedGamesField = "field_list=published_games"
+gamesGuidFilter = "filter=game:"
+companyCodePrefix = "3030-"
 formatJson = "format=json"
-api_key = "api_key=" + api_key_giantbomb.api_key
+apiKey = "api_key=" + api_key_giantbomb.api_key
 #required params
 headers={ "user-agent": "pyhton request module" }
+#One off
+gameID = "36765"
 
 
-#build api string
-get_publishedGames = base_url+moduleCompany+companyCodeTake2+"/?"+formatJson+"&"+fields+"&"+api_key
-print ("request url " + get_publishedGames)
+#build api string published games
+getPublishedGames = base_url+moduleCompany+companyCodeTake2+"/?"+formatJson+"&"+publishedGamesField+"&"+apiKey
+print ("request url " + getPublishedGames)
 
-#send get request
-query_response = requests.get(get_publishedGames, headers=headers)
-# pprint.pprint(query_response.json())
-
-output_dict = query_response.json()
+#send get request for published games
+responsePublishedGames = requests.get(getPublishedGames, headers=headers)
+# pprint.pprint(responsePublishedGames.json())
 
 
-### Sample parsing examples
-#returns 108 items
-print(len(output_dict['results']['published_games']))
+#build api string game reviews by ID
+getGameReviews = base_url+moduleReviews+"/?"+formatJson+"&"+gamesGuidFilter+companyCodePrefix+gameID+"&"+apiKey
+print ("request url " + getGameReviews)
 
-#returns first item as string
-print(output_dict['results']['published_games'][0])
-
-#returns first item's url
-print(output_dict['results']['published_games'][0]['api_detail_url'])
-
-#returns first item's id
-print(output_dict['results']['published_games'][0]['id'])
-
-#returns first item's game name
-print(output_dict['results']['published_games'][0]['name'])
+#send get request for game reviews by ID
+responseGameReviews = requests.get(getGameReviews, headers=headers)
 
 
 
 
+### Sample parsing examples GAME REVIEWS
+gameReviews_dict = responseGameReviews.json()
+#print(gameReviews_dict['results'][0]['score'])
+
+# construct review dictionary
+gameTitle = gameReviews_dict['results'][0]['game']['name']
+gameReviewDictionary = {}
+gameReviewDictionary =  {'GameTitle': gameTitle, 'ReviewsByScore': []}
+
+for n in range(len(gameReviews_dict['results'])):
+    
+    reviewScore = gameReviews_dict['results'][n]['score']
+    
+    reviewTitle = gameReviews_dict['results'][n]['deck']
+    reviewReviewer = gameReviews_dict['results'][n]['reviewer']
+    reviewDate = gameReviews_dict['results'][n]['date_added']
+    reviewIsDLC = gameReviews_dict['results'][n]['dlc']
+    
+    blockOfReviewData = {'Title' : reviewTitle, 'Reviewer' : reviewReviewer, 'ReleaseDate' : reviewDate, 'IsDLC' : reviewIsDLC}
+    
+    if reviewScore not in gameReviewDictionary['ReviewsByScore']:
+        gameReviewDictionary['ReviewsByScore'].append(reviewScore)
+    
+    #gameReviewDictionary['ReviewsByScore'][0] = blockOfReviewData
+    
+print(gameReviewDictionary)
+    #gameReviewDictionary['ReviewsByScore'][reviewScore] = blockOfReviewData
+    
+    
+
+    # gameReviewDictionary['ReviewsByScore'][reviewScore-1] = [gameReviews_dict['results'][n]['deck']]
+    # print(gameReviewDictionary['ReviewsByScore'][reviewScore-1])
+
+#print(gameReviewDictionary)
 
 
-#https://www.giantbomb.com/api/company/3010-450/?format=json&field_list=published_games
-#https://www.giantbomb.com/api/company/3010-450/?format=json&field_list=published_games&api_key=1c6309afefdb77a9ba01da868ed7afc3b3d140e7
+    
+
+
+
+#------------------------------------------------------
+
+# ### Sample parsing examples PUBLISHED GAMES
+# publishedGames_dict = responsePublishedGames.json()
+
+# #returns 108 items
+# print(len(publishedGames_dict['results']['published_games']))
+
+# #returns first item as string
+# print(publishedGames_dict['results']['published_games'][0])
+
+# #returns first item's url
+# print(publishedGames_dict['results']['published_games'][0]['api_detail_url'])
+
+# #returns first item's id
+# print(publishedGames_dict['results']['published_games'][0]['id'])
+
+# #returns first item's game name
+# print(publishedGames_dict['results']['published_games'][0]['name'])
+
+
+
+
+
+
+
+
+# https://www.giantbomb.com/api/user_reviews/?format=json&filter=game:3030-36765 ----------------REVIEWS OF THE GAME
+# https://www.giantbomb.com/api/company/3010-450/?format=json&field_list=published_games
+# https://www.giantbomb.com/api/company/3010-450/?format=json&field_list=published_games&api_key=1c6309afefdb77a9ba01da868ed7afc3b3d140e7
+# https://www.giantbomb.com/api/reviews/?format=json&filter=guid:3030-20827
+
+
+
+# {"$LabelName": {"ReviewsByScore": [
+#       "5": [ {"Title": "Title Name", "Reviewer": "Reviewer Name", "ReleaseDate": "01/01/2019"}],
+#       "4": [ {"Title": "Title Name", "Reviewer": "Reviewer Name", "ReleaseDate": "01/01/2019", "IsDlc": true}]
+#     ]
+#   }
+# }
